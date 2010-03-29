36cfdc800dac1557a9177f09bd304166f0fa9059
C. Titus Brown <titus@idyll.org>
2010-03-29 01:10:19 -0400

    fixed build-indexes file

--- Pyrex/data/build-indexes.sh
+++ Pyrex/data/build-indexes.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
-rm -fr data/cache/
-mkdir data/cache
-./Caper -s data/cache/ -g data/REL606.gmc.fa -m data/REL606-maq-map.txt < /dev/null
+export PYTHONPATH=/u/t/dev/screed
+
+CAPER=../CaperGCC/Caper
+CACHE=data/cache
+
+rm -fr ${CACHE}
+mkdir ${CACHE}
+
+${CAPER} indexgenome -g data/REL606.gmc.fa -o ${CACHE}
+
+${CAPER} indexmappings -m data/REL606-maq-map.txt -t mapview -o ${CACHE}
 
 # test!
-echo rel606:0:20:p |
-./Caper -i data/cache/saved.index -f data/cache/saved.refgenomeindex -M data/REL606-maq-map.txt -G data/REL606.gmc.fa
+echo rel606:0:20:p | \
+${CAPER} interactive -f ${CACHE}/REL606.gmc.fa.genomeindex -g ${CACHE}/REL606.gmc.fa.indexed -m data/REL606-maq-map.txt  -i ${CACHE}/REL606-maq-map.txt.mappingindex
 
 ###
 
 # also build the screed indexes
-python -m screed.fqdbm 
-data/REL606-seqs.fastq
+type python
+python -m screed.fqdbm data/REL606-seqs.fastq
